d797b5826dc08fb1283b0d2e1014d77e19909c49
Partho Ghosh <partho.ghosh24@gmail.com>
2026-07-02 14:58:03 -0700

    Update Director.h

--- Narrative/Director/Director.h
+++ Narrative/Director/Director.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <iostream>
+#include <map>
+#include <vector>
+class Director
+{
+public:
+  Director();
+  ~Director();
+private:
+
+  // This method will initialize all game related data
+  void InitializeGame();
+
+  // Room map contains the directed graph in which key is the source room id and
+  // value is the list of destination rooms.
+  std::map<std::string, std::vector<std:string>> roomMap = {}
+  
+};
